d1c5dcb0142a45a53a24ff20cda740311e10e01f
Noah Kobayashi <vohedge@gmail.com>
2022-01-28 20:38:24 +0900

    update for macos

---
+++ macos/use-bash.sh
@@ -0,0 +1,2 @@
+chsh -s /bin/bash
+echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> ~/.bash_profile
--- setup.sh
+++ setup.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 
 ##
-# Ansible for self build
-# 
-# CentOS
-# yum install -y epel-release
-# sed -i "s/enabled=1/enabled=0/g" /etc/yum.repos.d/epel.repo
-# yum install -y ansible --enablerepo=epel
-#
-# Ubuntu
-# sudo apt-get update
-# sudo apt-get install software-properties-common
-# sudo apt-add-repository ppa:ansible/ansible
-# sudo apt-get update
-# sudo apt-get install ansible
-# ansible-playbook "${PWD}/ansible-playbook/dev-machine.yml" --connection=local
-
-##
 # Synbolic links for dotfiles
 echo " * Make synbolic links for dotfiles"
 
@@ -50,19 +34,6 @@ init_dotfile "${HOME}/.vimrc"  "${PWD}/vim/.vimrc"
 init_dotfile "${HOME}/.gvimrc" "${PWD}/vim/.gvimrc"
 
 ##
-# ZSH
-
-# oh_my_zsh_path="${HOME}/.oh-my-zsh"
-# if [ ! -e ${oh_my_zsh_path} ]; then
-#   git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-# else
-#   echo " * Oh-My-Zsh is already installed"
-# fi
-
-# init_dotfile "${oh_my_zsh_path}/custom" "${PWD}/zsh/oh-my-zsh-custom"
-# init_dotfile "${HOME}/.zshrc" "${PWD}/zsh/.zshrc"
-
-##
 # BASH
 init_dotfile "${HOME}/.bashrc" "${PWD}/bash/.bashrc"
 
@@ -72,7 +43,7 @@ init_dotfile "${HOME}/.tmux.conf" "${PWD}/tmux/.tmux.conf"
 
 ##
 # GIT
-init_dotfile "${HOME}/.gitconfig" "${PWD}/git/.gitconfig"
+# init_dotfile "${HOME}/.gitconfig" "${PWD}/git/.gitconfig"
 
 ##
 # TIG
