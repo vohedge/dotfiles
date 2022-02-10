1fd76cefd16e1c5fc42eb9f9d1a2a19104f13ddf
Noah Kobayashi <vohedge@gmail.com>
2022-02-10 10:02:38 +0900

    Add git common settings script

---
+++ git/common-settings.sh
@@ -0,0 +1,5 @@
+git config --global core.editor 'vim -c "set fenc=utf-8"'
+git config --global init.defaultBranch main
+git config --global color.diff auto
+git config --global color.status auto
+git config --global color.branch auto
